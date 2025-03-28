627985512692acb19cf914eb8ece8c7fc93f2d27
YSpoof <32226366+YSpoof@users.noreply.github.com>
2025-03-28 20:32:20 -0300

    Feat: Transfer endpoint + PoC
    Debug option to add money
    
    Transfer model so we track money send/receive
    
    Minor adjusts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -61,8 +61,9 @@ export const routes: Routes = [
         loadComponent() {
           return import(
             './components/dashboard/transfer/transfer.component'
-          ).then((m) => m.TransferComponent);
+          ).then((m) => m.DashboardTransferComponent);
         },
+        canActivate: [AuthRequired],
       },
       {
         path: 'settings',
@@ -71,6 +72,7 @@ export const routes: Routes = [
             './components/dashboard/settings/settings.component'
           ).then((m) => m.SettingsComponent);
         },
+        canActivate: [AuthRequired],
       },
     ],
   },
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
-import { afterNextRender, Component, inject, signal } from '@angular/core';
+import { afterNextRender, Component, inject } from '@angular/core';
 import { ToastService } from '../../services/toast.service';
 import { UserService } from '../../services/user.service';
-import { AccountResponse } from '../../types/api';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,27 +8,23 @@ import { AccountResponse } from '../../types/api';
   template: `
     <div class="flex flex-col items-center justify-center h-screen">
       <p class="text-2xl font-bold mb-4">Dashboard</p>
-      @if (account() === null) {
+      @if (this.userSvc.account() === null) {
       <p class="text-lg">Dados da conta não disponíveis...</p>
       } @else {
-      <ul>
-        @if (!account()!.suspended) {
-        <li>
-          Chave: {{ account()!.pixi }} | Saldo:
-          {{ account()!.balance }}
-        </li>
+      <div>
+        @if (!this.userSvc.account()!.suspended) {
+        <p>
+          Chave: {{ this.userSvc.account()!.pixi }} | Saldo: F$
+          {{ this.userSvc.account()!.balance }}
+        </p>
         } @else {
-        <li>Conta suspensa com saldo: {{ account()!.balance }}</li>
+        <p>
+          Conta suspensa com saldo: F$ {{ this.userSvc.account()!.balance }}
+        </p>
         }
-      </ul>
+      </div>
       }
 
-      <button
-        (click)="loadAccountData()"
-        class="transition-all cursor-pointer bg-blue-500 hover:bg-blue-700 active:scale-95 text-white font-bold py-2 px-4 rounded"
-      >
-        Carregar dados da conta
-      </button>
       <button
         (click)="logout()"
         class="transition-all cursor-pointer bg-yellow-500 hover:bg-yellow-700 active:scale-95 text-white font-bold py-2 px-4 rounded"
@@ -48,12 +43,15 @@ import { AccountResponse } from '../../types/api';
 export class DashboardPageComponent {
   userSvc = inject(UserService);
   toast = inject(ToastService);
-  account = signal<AccountResponse | null>(null);
 
   constructor() {
     afterNextRender(() => {
       this.loadAccountData();
     });
+  }
+
+  loadAccountData() {
+    this.userSvc.loadAccountData();
   }
 
   logout() {
@@ -79,19 +77,5 @@ export class DashboardPageComponent {
         },
       });
     }
-  }
-
-  loadAccountData() {
-    this.account.set(null);
-    this.userSvc.getAccountData().subscribe({
-      next: (res) => {
-        this.account.set(res);
-        this.toast.showInfo('Dados da conta carregados com sucesso!');
-      },
-      error: (_err) => {
-        this.account.set(null);
-        this.userSvc.getNewToken();
-      },
-    });
   }
 }
